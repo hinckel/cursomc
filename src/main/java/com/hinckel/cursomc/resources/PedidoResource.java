8f29c5e73c636e42ee1d0ac0cd18fb19032c25f3
Hinckel <hinckel.luiz@gmail.com>
2018-06-24 16:43:09 -0300

    Endpoint /pedidos/{id} enabled

---
+++ src/main/java/com/hinckel/cursomc/resources/PedidoResource.java
@@ -0,0 +1,26 @@
+package com.hinckel.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hinckel.cursomc.domain.Pedido;
+import com.hinckel.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = service.find(id);
+		return ResponseEntity.ok(obj);
+	}
+	
+}

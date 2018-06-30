8047c51d99e92db84fd4bd2e504bcee612ac9307
Hinckel <hinckel.luiz@gmail.com>
2018-06-30 10:05:40 -0300

    Syntactic validation with bean validation

--- src/main/java/com/hinckel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/hinckel/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -33,14 +35,16 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria entity) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO obj) {
+		Categoria entity = service.fromDTO(obj);
 		entity = service.insert(entity);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(entity.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria entity) {
+	public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody CategoriaDTO objDto) {
+		Categoria entity = service.fromDTO(objDto);
 		entity.setId(id);
 		entity = service.update(entity);
 		return ResponseEntity.noContent().build();
